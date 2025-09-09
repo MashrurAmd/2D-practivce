b72ebe90966cee333e86f6fa669121513485184f
Mashrur Ahmed <mashrurahmed500@gmail.com>
2025-09-09 17:30:48 +0600

    s

--- Assets/Script/A.cs
+++ Assets/Script/A.cs
@@ -6,4 +6,12 @@ public class A : MonoBehaviour
 {
     public GameObject capsule;
 
+    //public static GameController gameController;
+
+
+    public void Start()
+    {
+        Instantiate(capsule,Vector3.zero,Quaternion.identity);
+    }
+
 }
